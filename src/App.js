587581649987e390d02bcfb1fa537690f4008946
Vyshniakov Ihor <gramazekavip@gmail.com>
2021-11-01 18:42:32 +0200

    added previous functionality to wrapped component(adding tracks after input and button click to store->re-render component due to different props that came as state from store)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useRef } from 'react'
 import { connect } from 'react-redux'
 // connect is function decorator
 
 function App(props) {
-	const { testStore } = props
+	const trackInput = useRef()
+	const { testStore, onAddTrack } = props
 	console.log(testStore)
+
+	const addTrack = () => {
+		onAddTrack(trackInput.current.value)
+		trackInput.current.value = ''
+	}
 
   return (
     <>
-			<form onSubmit={() => false}>
-				<input type="text"/>
+			<form onSubmit={e => e.preventDefault()}>
+				<input type="text" ref={trackInput}/>
 				<button
 					type="submit"
+					onClick={addTrack}
 				>
 					Add track
 				</button>
@@ -28,5 +36,10 @@ export default connect(
 	state => ({
 		testStore: state
 	}), // 1st argue - mapStateToProps
-	dispatch => ({})
+	dispatch => ({
+		// method onAddTrack will be available at props of component
+		onAddTrack: (trackName) => {
+			dispatch({ type: 'ADD_TRACK', payload: trackName })
+		}
+	}) // 2nd argue - mapDispatchToProps
 )(App);
